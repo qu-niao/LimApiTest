1976726db302572354defc72e74328ca678d5b94
qn <taoj>
2023-03-01 16:53:52 +0800

    项目表格样式优化

--- lim-web/src/pages/project/columns.tsx
+++ lim-web/src/pages/project/columns.tsx
@@ -2,24 +2,24 @@ export const columns = [
   {
     title: '项目名称',
     dataIndex: 'name',
-    width: 100,
+    width: '50%',
   },
   {
     title: '创建时间',
     dataIndex: 'created',
     valueType: 'dateTime',
-    width: 200,
+    width: '15%',
   },
   {
     title: '修改时间',
     dataIndex: 'updated',
-    width: 200,
+    width: '15%',
     valueType: 'dateTime',
   },
   {
     title: '操作',
     dataIndex: 'option',
-    width: 120,
+    width: '20%',
     valueType: 'option',
   },
 ];
