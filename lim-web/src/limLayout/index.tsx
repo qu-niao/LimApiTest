37a61bcbfcb91fe3c3ffb5e190e9a298590638f6
qn <3629182836@qq.com>
2023-03-10 11:05:13 +0800

    存储库地址修改

--- lim-web/src/limLayout/index.tsx
+++ lim-web/src/limLayout/index.tsx
@@ -67,7 +67,7 @@ const BasicLayout: React.FC<{}> = (props: any) => {
             <Button
               style={{ marginLeft: 8, position: 'relative', top: 2.6 }}
               icon={<GithubOutlined style={{ fontSize: 20 }} />}
-              onClick={() => window.open('https://github.com/qu-niao/LessIsMore')}
+              onClick={() => window.open('https://github.com/qu-niao/LimApiTest')}
             />
           </Tooltip>
         </span>
